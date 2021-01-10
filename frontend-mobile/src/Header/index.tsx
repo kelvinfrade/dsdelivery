59ae9a5da177309576b99cc8426579781b67a7dc
kelvinfrade <kelvinfrade@gmail.com>
2021-01-10 13:15:59 -0300

    frontend-mobile implementation

---
+++ frontend-mobile/src/Header/index.tsx
@@ -0,0 +1,43 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableWithoutFeedbackBase } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+
+function Header() {
+
+    const navigation = useNavigation();
+
+    const handleOnPress = () => {
+        navigation.navigate('Home');
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={handleOnPress}>
+            <View style={styles.container}>
+                <Image source={require('../assets/logo.png')} />
+                <Text style={styles.text}>DS Delivery</Text>
+            </View>
+        </TouchableWithoutFeedback>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#DA5C5C',
+        height: 80,
+        paddingTop: 35,
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        lineHeight: 25,
+        letterSpacing: -0.24,
+        color: '#FFF',
+        marginLeft: 13,
+        fontFamily: 'OpenSans_700Bold'
+    }
+});
+
+export default Header;
